0d8ced8d3353e5dc746de8d44e8809f8c0fc0700
pvelp <72802588+pvelp@users.noreply.github.com>
2022-03-27 17:18:09 +0300

    v0.1.0

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,92 @@
-#include <example.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-int main() {
-  example();
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+
+// Performs an HTTP GET and prints the response
+int main(int argc, char** argv)
+{
+  try
+  {
+    // Check command line arguments.
+    if(argc != 5)
+    {
+      std::cerr << "Usage: http-client-sync <host> <port> <target> "
+                       "<request>\n"
+                << "Example:\n"
+                   "localhost 8080 /v1/api/suggest "
+                << R"({"input":"<user_input>"})"
+                   "\n" << argv[1] << " " << argv[2] << " " << argv[3] << " " << argv[4];
+      return EXIT_FAILURE;
+    }
+    auto const host = argv[1];
+    auto const port = argv[2];
+    auto const target = argv[3];
+    auto const request = argv[4];
+    int version = 11;
+    std::cout << host << " " << port << " " << target << " " <<  request << std::endl;
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // These objects perform our I/O
+    tcp::resolver resolver(ioc);
+    beast::tcp_stream stream(ioc);
+
+    // Look up the domain name
+    auto const results = resolver.resolve(host, port);
+
+    // Make the connection on the IP address we get from a lookup
+    stream.connect(results);
+
+    http::string_body::value_type body = request;
+
+    // Set up an HTTP GET request message
+    http::request<http::string_body> req{http::verb::post, target, version};
+    req.set(http::field::host, host);
+    req.body() = body;
+    req.prepare_payload();
+    req.set(http::field::content_type, "text/html");
+    // Send the HTTP request to the remote host
+    http::write(stream, req);
+
+    // This buffer is used for reading and must be persisted
+    beast::flat_buffer buffer;
+
+    // Declare a container to hold the response
+    http::response<http::dynamic_body> res;
+
+    // Receive the HTTP response
+    http::read(stream, buffer, res);
+
+    // Write the message to standard out
+    std::cout << res << std::endl;
+
+    // Gracefully close the socket
+    beast::error_code ec;
+    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+    // not_connected happens sometimes
+    // so don't bother reporting it.
+    //
+    if(ec && ec != beast::errc::not_connected)
+      throw beast::system_error{ec};
+
+    // If we get here then the connection is closed gracefully
+  }
+  catch(std::exception const& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
+
+//]
--- include/header.hpp
+++ include/header.hpp
@@ -3,6 +3,4 @@
 #ifndef INCLUDE_EXAMPLE_HPP_
 #define INCLUDE_EXAMPLE_HPP_
 
-auto example() -> void;
-
 #endif // INCLUDE_EXAMPLE_HPP_
