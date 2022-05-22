b83d3fbb5cd9d53e92b052c5e8eaa6031fdd14a3
kyzminyarik <Yarosav_kuzmin2002@mail.ru>
2022-05-22 09:52:43 -0700

    commit1

--- demo/main.cpp
+++ demo/main.cpp
@@ -6,17 +6,17 @@
 #include <iostream>
 #include <string>
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
 
-// Performs an HTTP GET and prints the response
+// Выполняет HTTP GET и печатает ответ
 int main(int argc, char** argv)
 {
   try
   {
-    // Check command line arguments.
+    // Проверят аргументы командной строки.
     if(argc != 5)
     {
       std::cerr << "Usage: http-client-sync <host> <port> <target> "
@@ -24,7 +24,8 @@ int main(int argc, char** argv)
                 << "Example:\n"
                    "localhost 8080 /v1/api/suggest "
                 << R"({"input":"<user_input>"})"
-                   "\n" << argv[1] << " " << argv[2] << " " << argv[3] << " " << argv[4];
+                   "\n" << argv[1] << " " << argv[2] << " "
+                << argv[3] << " " << argv[4];
       return EXIT_FAILURE;
     }
     auto const host = argv[1];
@@ -32,54 +33,51 @@ int main(int argc, char** argv)
     auto const target = argv[3];
     auto const request = argv[4];
     int version = 11;
-    std::cout << host << " " << port << " " << target << " " <<  request << std::endl;
-    // The io_context is required for all I/O
+    std::cout << host << " " << port << " " << target
+              << " " <<  request << std::endl;
+    //io_context требуется для всех операций ввода-вывода
     net::io_context ioc;
 
-    // These objects perform our I/O
+    // Эти объекты выполняют наш ввод-вывод
     tcp::resolver resolver(ioc);
     beast::tcp_stream stream(ioc);
 
-    // Look up the domain name
     auto const results = resolver.resolve(host, port);
 
-    // Make the connection on the IP address we get from a lookup
-    stream.connect(results);
+    // Установка соединение по IP-адресу, который мы получаем из
+    // поиска stream.connect(results);
 
     http::string_body::value_type body = request;
 
-    // Set up an HTTP GET request message
-    http::request<http::string_body> req{http::verb::post, target, version};
+    // Настройка HTTP-запроса на получение сообщения
+    http::request<http::string_body> req{http::verb::post,
+                                         target, version};
     req.set(http::field::host, host);
     req.body() = body;
     req.prepare_payload();
     req.set(http::field::content_type, "text/html");
-    // Send the HTTP request to the remote host
+
+    // Отправка HTTP-запрос на удаленный хост
     http::write(stream, req);
 
-    // This buffer is used for reading and must be persisted
+    // Этот буфер используется для чтения и должен быть сохранен
     beast::flat_buffer buffer;
 
-    // Declare a container to hold the response
+    // Объявление контейнера для хранения ответа
     http::response<http::dynamic_body> res;
 
-    // Receive the HTTP response
+    // получение ответа
     http::read(stream, buffer, res);
 
-    // Write the message to standard out
+
     std::cout << res << std::endl;
 
-    // Gracefully close the socket
+    // закрытие сокета
     beast::error_code ec;
     stream.socket().shutdown(tcp::socket::shutdown_both, ec);
 
-    // not_connected happens sometimes
-    // so don't bother reporting it.
-    //
     if(ec && ec != beast::errc::not_connected)
       throw beast::system_error{ec};
-
-    // If we get here then the connection is closed gracefully
   }
   catch(std::exception const& e)
   {
@@ -88,5 +86,3 @@ int main(int argc, char** argv)
   }
   return EXIT_SUCCESS;
 }
-
-//]
